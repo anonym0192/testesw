443778305488309a33102e85274c517fd572f318
Anonimo192 <shakunin69@gmail.com>
2020-09-01 14:46:39 -0300

    cache creation

--- sw.js
+++ sw.js
@@ -2,9 +2,25 @@ self.addEventListener('install', function(event){
 	console.log("installing the Service Worker");
 	self.skipWaiting();
 
+	event.waitUntil(caches.open('teste-v1').then(cache => {
+		cache.add('fox.jpg');
+	}));
+
 });
 
 self.addEventListener('activate', function(event){
-
 	console.log("activating service worker");
+});
+
+self.addEventListener('fetch', function(event){
+
+	const url = new URL(event.request.url);
+
+	console.log("domain :"+url.origin);
+	console.log("pathname :"+url.pathname);
+	console.log("Request URL :"+event.request.url);
+
+	if(location.origin == url.origin && url.pathname == '/testesw/doomsday.jpg'){
+		event.respondWith(caches.match('/testesw/fox.jpg'));
+	}
 });
